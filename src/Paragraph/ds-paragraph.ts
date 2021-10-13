1d13f0901f8a755efc8b023bec745bf48186c259
Abner Simões <abner.simoes@hotmail.com>
2021-10-13 17:02:47 -0300

    refactor: styles file

--- src/Paragraph/ds-paragraph.ts
+++ src/Paragraph/ds-paragraph.ts
@@ -1,33 +1,11 @@
-import { html, LitElement, css } from 'lit';
+import { html, LitElement } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
 import * as Types from '../types';
+import styles from './styles';
 
 @customElement('ds-paragraph')
 export class Paragraph extends LitElement {
-  static styles = css`
-    :host {
-      display: block;
-    }
-
-    p {
-      font-weight: var(--asset-font-light-weight);
-    }
-
-    .small {
-      font-size: var(--size-font-xxxs);
-      margin-bottom: var(--size-spacing-outset-xxxs);
-    }
-
-    .medium {
-      font-size: var(--size-font-xs);
-      margin-bottom: var(--size-spacing-outset-xxs);
-    }
-
-    .large {
-      font-size: var(--size-font-sm);
-      margin-bottom: var(--size-spacing-outset-xs);
-    }
-  `;
+  static styles = styles;
 
   @property({ type: Types.Size }) size = Types.Size.Medium;
 
