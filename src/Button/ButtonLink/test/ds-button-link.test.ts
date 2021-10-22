a4bdd1b2447c29da56175724050a6ce296aeb60b
Abner Simões <abner.simoes@hotmail.com>
2021-10-22 15:02:45 -0300

    fix: tests

--- src/Button/ButtonLink/test/ds-button-link.test.ts
+++ src/Button/ButtonLink/test/ds-button-link.test.ts
@@ -2,26 +2,37 @@ import { expect, fixture, elementUpdated } from '@open-wc/testing';
 import { html } from 'lit';
 import * as Types from '../../../types';
 import { ButtonLink } from '..';
+import '../../../Icon/ds-icon';
 import '../ds-button-link';
 
 describe('Button Icon', () => {
   let component: ButtonLink;
   let element: HTMLElement;
+  let iconLeft: HTMLElement;
+  let iconRight: HTMLElement;
 
   beforeEach(async () => {
     component = await fixture<ButtonLink>(
-      html`<ds-button-link to="/foo">bar</ds-button-link>`
+      html`<ds-button-link to="/foo">
+        <ds-icon slot="icon-left" name="navigate_before"></ds-icon>
+        bar
+        <ds-icon slot="icon-right" name="navigate_next"></ds-icon
+      ></ds-button-link>`
     );
     element = component.shadowRoot!.querySelector('a')!;
+    iconLeft = element.querySelector('slot[name=icon-left]')!;
+    iconRight = element.querySelector('slot[name=icon-right]')!;
   });
 
   it('should be render a medium link button default', async () => {
     expect(element).to.exist;
+    expect(iconLeft).to.exist;
+    expect(iconRight).to.exist;
     expect(element).to.have.class('medium');
     expect(element).not.to.have.class('outline');
     expect(element).not.to.have.class('raised');
     expect(element).to.have.property('href', 'http://localhost:8000/foo');
-    expect(component.textContent).to.equal('bar');
+    expect(component.textContent).to.string('bar');
   });
 
   it('should be render a large outline disabled link button by properties', async () => {
--- src/Button/test/ds-button.test.ts
+++ src/Button/test/ds-button.test.ts
@@ -2,25 +2,38 @@ import { expect, fixture, elementUpdated } from '@open-wc/testing';
 import { html } from 'lit';
 import sinon from 'sinon';
 import * as Types from '../../types';
+import '../../Icon/ds-icon';
 import '../ds-button';
 import { ButtonBase } from '../ds-button.base';
 
 describe('Button', () => {
   let component: ButtonBase;
   let element: HTMLElement;
+  let iconLeft: HTMLElement;
+  let iconRight: HTMLElement;
 
   beforeEach(async () => {
-    component = await fixture<ButtonBase>(html`<ds-button>Foo bar</ds-button>`);
+    component = await fixture<ButtonBase>(html`
+      <ds-button>
+        <ds-icon slot="icon-left" name="navigate_before"></ds-icon>
+        Foo bar
+        <ds-icon slot="icon-right" name="navigate_next"></ds-icon>
+      </ds-button>
+    `);
     element = component.shadowRoot!.querySelector('button')!;
+    iconLeft = element.querySelector('slot[name=icon-left]')!;
+    iconRight = element.querySelector('slot[name=icon-right]')!;
   });
 
   it('should be render a medium button by default', async () => {
     expect(element).to.exist;
+    expect(iconLeft).to.exist;
+    expect(iconRight).to.exist;
     expect(element).to.have.class('medium');
     expect(element).not.to.have.class('outline');
     expect(element).not.to.have.class('raised');
     expect(element).to.have.property('disabled', false);
-    expect(component.textContent).to.equal('Foo bar');
+    expect(component.textContent).to.string('Foo bar');
   });
 
   it('should be render a large outline disabled button by properties', async () => {
